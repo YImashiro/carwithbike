dd7b2d1e5cdecc61ac5e640aca645fc89e01948c
YImashiro <imashiro304@gmail.com>
2017-12-21 22:06:31 +0900

    change MotorDriver

--- Parts/MotorDriver.py
+++ Parts/MotorDriver.py
@@ -9,15 +9,29 @@ class MotorDriver:
         channels = [2,3]
         GPIO.setup(channels,GPIO.OUT)
 
+    def __setting(self):
+        GPIO.output(self.channels,GPIO.LOW)
+        sleep(0.0001)
+        
     def goFoward(self, duty):
+        self.__setting()
         pwm = GPIO.PWM(2,self.freq)
-        pwm.start(duty)
+        pwm.start(self.duty)
 
     def goBackward(self,duty):
+        self.__setting()
         pwm = GPIO.PWM(3,self.freq)
-        pwm.start(duty)
+        pwm.start(self.duty)
+
+    def turbo(self):
+        self.__setting()
+        pwm = GPIO.PWM(2,self.freq)
+        pwm.start(1.0)
+        sleep(1)
+        pwm.stop()
 
     def breaking(self):
+        self.__setting()
         GPIO.output(channels,GPIO.HIGH)
 
     def stop(self):
