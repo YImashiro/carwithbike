4c828f13a525a6e4b3b1bc693cd96948a2fe9fb4
YImashiro <imashiro304@gmail.com>
2017-12-23 06:53:52 +0900

    Light Completed

--- Parts/Light.py
+++ Parts/Light.py
@@ -2,18 +2,15 @@ import RPi.GPIO as GPIO
 
 class Light:
     freq = 1
-    duty = 0.5
+    duty = 50
     def __init__(self,pin1,pin2,pin3):
         self.channels = [pin1,pin2,pin3]
         GPIO.setup(self.channels,GPIO.OUT)
-        pwm_left = GPIO.PWM(pin1,self.freq)
-        pwm_right = GPIO.PWM(pin3,self.freq)
+        GPIO.output(self.channels,GPIO.LOW)
+        self.pwm_left = GPIO.PWM(self.channels[0],self.freq)
+        self.pwm_right = GPIO.PWM(self.channels[2],self.freq)        
 
-    def __setup(self):
-        GPIO.setup(self.channels,GPIO.OUT)
-        pwm_left = GPIO.pwm(self.channels[0],freq)
-        pwm_right = GPIO.pwm(self.channels[2],freq)
-        
+
     def headLampOn(self):
         GPIO.output(self.channels[1],GPIO.HIGH)
     def headLampOff(self):
@@ -30,24 +27,32 @@ class Light:
         self.pwm_right.stop() 
 
     def clean(self):
-        GPIO.cleanup(channels)
+        GPIO.cleanup(self.channels)
 
 def test():
     GPIO.setmode(GPIO.BCM)
     import time
-    light = Light(17,22,27)
-    print("headlamp on")
-    light.headLampOn()
-    time.sleep(5)
-    print("headlamp off")
-    light.headLampOff()
-    print("leftWinker On")
-    light.leftwinkerOn()
-    time.sleep(5)
-    print("right on, left Off")
-    light.rightWinkerOn()
-    time.sleep(5)
-    light.clean()
+    try:
+        light = Light(17,22,27)
+        '''print("headlamp on")
+        light.headLampOn()
+        time.sleep(5)
+        print("headlamp off")
+        light.headLampOff()
+        '''
+        print("right on, left Off")
+        light.rightWinkerOn()
+        time.sleep(5)
+        light.rightWinkerOff()
+        print("leftWinker On")
+        light.leftWinkerOn()
+        time.sleep(5)
+        light.leftWinkerOff()
+    
+    except:
+        pass
+    finally:
+        light.clean()
     
 if __name__ == "__main__":
     test()
