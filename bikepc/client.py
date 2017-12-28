2c19ce35aa274b11c601922f2581efb5a643e99f
YImashiro <imashiro304@gmail.com>
2017-12-28 15:31:51 +0900

    client

--- bikepc/client.py
+++ bikepc/client.py
@@ -2,11 +2,11 @@ import socket
 import time
 
 ADDRESS = '192.168.100.104'
-PORT = 8000
+PORT = 57214
 with socket.socket() as socket:
     socket.connect((ADDRESS,PORT))
     while True:
-        data = socket.recv(8)
+        data = socket.recv(3)
         if not data:
             break
         print(data.decode())
