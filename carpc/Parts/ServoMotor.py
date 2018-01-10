86a6bd844aefd6d538fda9827acad306a77c567d
YImashiro <imashiro304@gmail.com>
2018-01-11 05:41:13 +0900

    servo

--- carpc/Parts/ServoMotor.py
+++ carpc/Parts/ServoMotor.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-
+import time
 class ServoMotor:
     '''PWM period may be 50Hz(20ms) 
        Duty Cycle is 0.5~2.4ms ~0.5ms is -90deg /~2.4ms +90deg'''
@@ -19,22 +19,34 @@ class ServoMotor:
         self.left = self.lowest / self.period * 100
         self.straight = self.middle/ self.period * 100
         self.pwm.start(self.straight)
+        self.percentage = 0
 
     def __steeringControl(self,percentage):
         n = (self.right - self.straight)/100
         return percentage * n  
 
     def steeringControl(self,percentage):
-        self.pwm.ChangeDutyCycle(self.straight + self.__steeringControl(percentage))
-
+        print(percentage,self.percentage)
+        if percentage > self.percentage:
+            for i in range(self.percentage,percentage):
+                self.pwm.ChangeDutyCycle(self.straight + self.__steeringControl(i))
+                time.sleep(0.01)
+        elif percentage < self.percentage:
+            for i in range(self.percentage,percentage,-1):
+                self.pwm.ChangeDutyCycle(self.straight + self.__steeringControl(i))
+                time.sleep(0.01)
+        self.percentage = percentage
+        
     def turnLeft(self):
         self.steeringControl(-50)
     def turnRight(self):
         self.steeringControl(50)
 
     def servoReset(self):
-        self.pwm.ChangeDutyCycle(self.straight)
-
+        if self.percentage != 0:
+            self.steeringControl(0)
+            
+        
     def clean(self):
         GPIO.cleanup(self.pin)
     
